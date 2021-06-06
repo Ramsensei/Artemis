b1562d0a1366fd2c6f680057947b6920cbc339e6
valesskablanco <valesskablanco2@gmail.com>
2021-06-05 22:15:37 -0600

    Pequeño avance xD

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
 
+pygame.init()
+
 # Variables globales
 
 WIDTH,HEIGHT = 600,800
 FPS = 60
+VENTANA_PRINCIPAL = pygame.display.set_mode((WIDTH,HEIGHT))
 
 # Definiendo pantalla principal
 
-pygame.init()
+def main():
 
+    run = True
+
+    while run:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+
+                run = False
 
 if __name__ == "__main__":
-    pass
+    main()
 
-    
