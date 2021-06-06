223a01cf73f6db05f326c063c9d90b925d32b1f8
valesskablanco <valesskablanco2@gmail.com>
2021-06-05 21:24:00 -0600

    Actualización2

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pygame
 
+# Variables globales
+
+WIDTH,HEIGHT = 600,800
+FPS = 60
+
+# Definiendo pantalla principal
+
+pygame.init()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     pass
 
     
