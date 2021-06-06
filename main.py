5515b82f7fa29570e42f3a0c956563b9394bbab7
valesskablanco <valesskablanco2@gmail.com>
2021-06-06 00:17:40 -0600

    Ventana Principal

--- main.py
+++ main.py
@@ -7,22 +7,39 @@ pygame.init()
 WIDTH,HEIGHT = 600,800
 FPS = 60
 VENTANA_PRINCIPAL = pygame.display.set_mode((WIDTH,HEIGHT))
+WHITE = (255,255,255)
 
 # Definiendo pantalla principal
 
+pygame.display.set_caption("Artemis")
+
+def draw_main_window():
+
+    VENTANA_PRINCIPAL.fill(WHITE)
+    pygame.display.update()
+
+# Módulo principal
+
 def main():
 
+    clock = pygame.time.Clock()
     run = True
 
     while run:
 
+        clock.tick(FPS)
+        
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
 
                 run = False
 
+        draw_main_window()
+
+    pygame.quit()
+
 if __name__ == "__main__":
-    
+
     main()
 
