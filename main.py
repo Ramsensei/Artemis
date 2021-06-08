79403e1ee1e5a43e47dfc14f49345efcd0e8a174
Ramsensei <ramsensei@estudiantec.cr>
2021-06-08 01:04:17 -0600

    Creación de los archivos extra

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from os import path
 from pygame import mouse
-from pygame.locals import *
+from pygame.locals import MOUSEBUTTONDOWN
+from funciones import up_img, draw_button, play_song
 
 pygame.init()
 
@@ -15,56 +16,7 @@ BLACK = (0, 0, 0)
 GRAY = (64,64,64)
 FUENTE = pygame.font.SysFont("Times New Roman", 22)
 
-# Funciones para cargar archivos
-
-def up_img(name):
-
-    ruta = path.join("assets", name)
-    img = pygame.image.load(ruta)
-
-    return img
-
-# Funciones de reproducción de sonido
-
-pygame.mixer.init()
-
-def load_MP3(nombre):
-
-    return path.join("assets", nombre)
-
-
-def play_song(MP3):
-
-    stop_song()
-
-    pygame.mixer.music.load(load_MP3(MP3))
-    pygame.mixer.music.play(-1)
-
-def stop_song():
-
-    if pygame.mixer.music.get_busy():
-
-        pygame.mixer.music.stop()
-
-# Función para crear botones
-
-
-def draw_button(screen, button, palabra):
-
-    if button.collidepoint(pygame.mouse.get_pos()):
-
-        pygame.draw.rect(screen, GRAY, button, 0)
-
-    else:
-
-        pygame.draw.rect(screen, BLACK, button, 0)
-
-    txt = FUENTE.render(palabra, True, WHITE)
-    screen.blit(txt, (button.x+(button.width-txt.get_width())/2,
-                button.y+(button.height-txt.get_height())/2))
-
 # Definiendo pantalla principal
-
 
 pygame.display.set_caption("Artemis")
 
