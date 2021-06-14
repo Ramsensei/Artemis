05d606e575c2cf7599875e3b8b5f1995e9c2178f
Ramsensei <ramsensei@estudiantec.cr>
2021-06-14 10:09:59 -0600

    commit

--- Pruebas.py
+++
@@ -1,7 +0,0 @@
-import random
-import time
-while True:
-    # print("randint: ", random.randint(0, 1))
-    print("randrange: ", random.randrange(1, 6))
-    # print("choice: ", random.choice([2, 7, 4]))
-    time.sleep(0.5)
